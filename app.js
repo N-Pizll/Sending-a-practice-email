8e15d1668d9ffe7e36194f3b843018ddab81d951
Nima Khastar <nimapizll.nn@gmail.com>
2024-05-17 12:41:02 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
 const form = document.querySelector("form");
-const fullName =document.getElementById("name")
-const email =document.getElementById("name")
-const phone =document.getElementById("name")
-const subject =document.getElementById("name")
-const mess =document.getElementById("name")
+fullName = document.getElementById("name");
+email = document.getElementById("email");
+phone = document.getElementById("phone");
+subject = document.getElementById("subject");
+mess = document.getElementById("message");
 
+function sendEmail() {
+  const bodyMessage = `Full Name:${fullname.value}<br> Email:${email.value}<br> Phone Number: ${phone.value}<br> Message: ${mess.value}`;
 
-
-
-
-
-function sendemail() {
-    
   Email.send({
     Host: "smtp.elasticemail.com",
     Username: "nimakh388@gmail.com",
     Password: "7ABCC8733397569AB79EE14E9530BC9AB8B1",
-    To: "them@website.com",
-    From: "you@isp.com",
-    Subject: "This is the subject",
-    Body: "And this is the body",
+    To: "nimakh388@gmail.com",
+    From: "nimakh388@gmail.com",
+    Subject: subject.value,
+    Body: bodyMessage,
   }).then((message) => alert(message));
 }
 
-
 function chekInputs() {
-    const items =document.querySelectorAll(".item");
-    for(const item of items){
-        if (item.value==""){
-            item.classList.add("error");
-            item.parentElement.classList.add("error");
-        }
+  const items = document.querySelectorAll(".item");
+  for (const item of items) {
+    if (item.value == "") {
+      item.classList.add("error");
+      item.parentElement.classList.add("error");
     }
+  }
 }
 
-form.addEventListener("submit" ,(e)=>{
-    e.preventDefault()
-    sendemail()
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendemail();
+});
